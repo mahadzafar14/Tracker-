14bb67de7dfd416160c71ef0c85bb86c7d999494
mahadzafar14 <mahadzafar14@hotmail.co.uk>
2019-01-14 00:49:07 +0000

    a

--- app/src/main/java/com/example/mahadzafarahmed/tracker/ui/addingcalories/AddingcaloriesViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.mahadzafarahmed.tracker.ui.addingcalories;
-
-import android.arch.lifecycle.ViewModel;
-
-public class AddingcaloriesViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/mahadzafarahmed/tracker/ui/viewcalories/ViewCaloriesViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.mahadzafarahmed.tracker.ui.viewcalories;
-
-import android.arch.lifecycle.ViewModel;
-
-public class ViewCaloriesViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
